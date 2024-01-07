dad5c265c9e3fe85d95d8f6b047a7ff0cb6f7211
Rodrigo Kellermann Ferreira <rkferreira@gmail.com>
2024-01-07 15:38:11 -0300

    second batch

--- exercises/primitive_types/primitive_types2/main.go
+++ exercises/primitive_types/primitive_types2/main.go
@@ -1,11 +1,11 @@
 // primitive_types2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
+  var who string = "Me"
 	fmt.Printf("Hello, %s\n", who) // '%s' is used to format strings
 }
