8ae2863f7ba9224e2139f6f365e1e7230304d4a8
Rodrigo Kellermann Ferreira <rkferreira@gmail.com>
2024-01-19 20:13:58 -0300

    fourth batch

--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -1,15 +1,16 @@
 // structs1
 // Make me compile!
 //
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 type Person struct {
-
+  name string
+  age  int
 }
 
 func main() {
+  person := Person{"Bob", 20}
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,7 +1,6 @@
 // structs2
 // Make me compile!
 //
-// I AM NOT DONE
 package main
 
 import "fmt"
@@ -10,6 +9,11 @@ type Person struct {
 	// don't just create the phone field here. embed a new struct
 	name string
 	age int
+  ContactDetails
+}
+
+type ContactDetails struct {
+  phone string
 }
 
 func main() {
