f4f8deb132d2849706a225a8cb2c463b52aca77f
Rodrigo Kellermann Ferreira <rkferreira@gmail.com>
2024-01-22 20:24:44 -0300

    fifth batch

--- exercises/anonymous_functions/anonymous_functions2/main.go
+++ exercises/anonymous_functions/anonymous_functions2/main.go
@@ -1,15 +1,16 @@
 // anonymous functions2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	var sayBye func(name string)
+  var sayBye func(name string)
 
-	sayBye = func() {
+	sayBye = func(n string) {
 		fmt.Printf("Bye %s", n)
 	}
+
+  sayBye("Dave")
 }
